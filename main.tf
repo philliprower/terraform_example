f5e5aaf23ab07500914523d19ab49fddfc84b349
philliprower <phillip.rower@presidiohealth.com>
2017-10-26 15:35:41 -0700

    Update for cluster auto scale group

--- main.tf
+++ main.tf
@@ -2,18 +2,19 @@ provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-09d2fb69"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-09d2fb69"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    security_groups = ["${aws_security_group.instance.id}"]
+    
     user_data = <<-EOF
         #!/bin/bash
         echo "Hello, World" > index.html
         nohup busybox httpd -f -p "${var.server_port}" &
         EOF
 
-    tags {
-        Name = "terraform-example"
+    lifecycle {
+        create_before_destroy = true
     }
 }
 
@@ -26,13 +27,83 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    lifecycle {
+        create_before_destroy = true
+    }
 }
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_elb" "example" {
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}/"
+    }
+}
+
+data "aws_availability_zones" "all" {}
 
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
     default = 8080
 }
 
-output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
+output "availability_zones" {
+    value = "${data.aws_availability_zones.all.names}"
+}
+
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
